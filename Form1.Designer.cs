f75ed156a8840bd38068825a6a5b96bf96aa03b9
royyi666 <royyi@qq.com>
2021-02-08 12:26:19 +0800

    MDBx32

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,14 +32,26 @@ namespace NOPIxls
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column3,
+            this.Column1,
+            this.Column2});
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowTemplate.Height = 23;
             this.dataGridView1.Size = new System.Drawing.Size(776, 360);
             this.dataGridView1.TabIndex = 0;
@@ -62,12 +74,48 @@ namespace NOPIxls
             this.button2.TabIndex = 2;
             this.button2.Text = "NOPIxls";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(94, 381);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 21);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(94, 409);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 21);
+            this.dateTimePicker2.TabIndex = 4;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "编号";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "日期";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "日志";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 500;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
@@ -83,6 +131,11 @@ namespace NOPIxls
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -19,6 +19,59 @@ namespace NOPIxls
 
         private void button1_Click(object sender, EventArgs e)
         {
+            try
+            {
+                DateTime starttime, endtime;
+
+                this.dataGridView1.DataSource = null;
+                this.dataGridView1.Rows.Clear();
+
+                starttime = Convert.ToDateTime(this.dateTimePicker1.Text + " 0:00:00");//起始时间
+                endtime = Convert.ToDateTime(this.dateTimePicker2.Text + " 23:59:59"); ;//结束时间
+
+                string strsel = "";
+                int n = 0;
+
+                strsel = "Select * from SystemLog where LogTime between #" + starttime + "# and #" + endtime + "#";
+               
+
+                if (strsel != "")
+                {
+                    
+
+                    OleDbConnection con = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;data source=MainData.mdb;Jet OLEDB:database password=jyxjdz");
+                    OleDbCommand cmd = new OleDbCommand(strsel, con);
+                    OleDbDataAdapter ada = new OleDbDataAdapter(cmd);
+                    OleDbDataReader rd;
+                    con.Open();
+                    rd = cmd.ExecuteReader();
+                    while (rd.Read())
+                    {
+                        this.dataGridView1.Rows.Add();
+                        this.dataGridView1.Rows[n].Cells[0].Value = rd[0].ToString();
+                        this.dataGridView1.Rows[n].Cells[1].Value = rd[1].ToString();
+                        this.dataGridView1.Rows[n].Cells[2].Value = rd[2].ToString();
+                        n += 1;
+                    }
+                    rd.Close();
+                    con.Close();
+                }
+                else
+                {
+                    MessageBox.Show("未选择查询条件，无法获取信息", "提示");
+                    this.dataGridView1.DataSource = null;
+                    this.dataGridView1.Rows.Clear();
+                    //this.dataGridView2.Columns.Clear();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
 
         }
     }
